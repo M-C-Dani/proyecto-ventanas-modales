75544cd28358898e8ab95ffa118e702deb3465bb
M-C-Dani <martincastillodan@gmail.com>
2024-02-07 13:15:08 +0100

    asociamos un listener para cada evento que suceda en la interacción de las ventanas

--- script.js
+++ script.js
@@ -17,4 +17,15 @@ const closeModal = function () {
     overlay.classList.add('hidden'); // Añade la clase 'hidden' para ocultar la superposición
 };
 
+// Asociamos un listener a cada botón para abrir el modal
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+// Asociamos un listener al botón de cerrar modal para cerrar el modal
+btnCloseModal.addEventListener('click', closeModal);
+
+// Asociamos un listener a la superposición para cerrar el modal cuando se hace clic fuera del mismo
+overlay.addEventListener('click', closeModal);
+
 
